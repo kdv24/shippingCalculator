f30896d7d684a2ff7474eb7e97d587e39c54e9cd
Kelly de Vries <kdv@me.com>
2015-02-10 16:37:11 -0800

    completed math
    added in pretend Lucky Number shipping cost generator based on random
    number

--- calculator-results.php
+++ calculator-results.php
@@ -1,16 +1,21 @@
 <?php 
-    $first_number = $_GET["first_number"];
-    $second_number = $_GET["second_number"]; 
-    $solution = $first_number + $second_number;
+    $weight = $_GET["weight"];
+    $distance = $_GET["distance"]; 
+    $shippingCost = (($weight/20) + ($distance/20));
+    $groundWeight = ($distance/$weight);
+    $randomNumber = rand(-5, 5);
+    $luckyNumber = ($shippingCost*$randomNumber);
 ?>
 <!DOCTYPE html>
 <html>
 <head>
     <title>Your Answer Below</title>
 </head>
+<script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script>
 <body>
-    <h1>Arithmetic in PHP is easy!</h1>
-    <p>The sum of <?php echo $first_number; ?> and <?php echo $second_number; ?> is: </p>
-    <p><?php echo $solution; ?></p>
+    <h1>Shipping Calculation</h1>
+    <p>The total shipping cost of mailing your package weighing <?php echo $weight; ?>, a distance of <?php echo $distance; ?> is: $<?php echo $shippingCost; ?>.</p>
+    <p>Alternatively, you could ship it ground rate for $<?php echo $groundWeight; ?>.</p>
+    <p>Or, a final option is the "Do you feel lucky" method of calculation, which this time results in a final cost of $<?php echo $luckyNumber; ?>.</p>
 </body>
 </html>
